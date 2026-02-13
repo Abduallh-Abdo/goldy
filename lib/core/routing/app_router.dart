0392d4e41906516d5137faac57d53a90e1d19529
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-02-14 01:04:53 +0200

    add navogation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:goldy/features/gold/presentation/screens/gold_screen.dart';
+import 'package:goldy/features/silver/presentation/screens/silver_screen.dart';
 
 import '../constant/app_strings.dart';
 import '../presentaion/home_screen.dart';
@@ -7,16 +9,18 @@ import 'routes.dart';
 class AppRouter {
   Route onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-     case Routes.homeScreen:
-        return MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
-        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (context) => const HomeScreen());
+      case Routes.goldScreen:
+        return MaterialPageRoute(builder: (context) => const GoldScreen());
+      case Routes.silverScreen:
+        return MaterialPageRoute(builder: (context) => const SilverScreen());
       default:
         return undefinedRoute();
     }
   }
 
-    static Route<dynamic> undefinedRoute() {
+  static Route<dynamic> undefinedRoute() {
     return MaterialPageRoute(
       builder: (context) =>
           const Scaffold(body: Center(child: Text(AppStrings.noRouteFound))),
