419acf4173e6d16bfe451397ce568af90a41bb08
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-02-14 01:04:38 +0200

    add navigation

--- lib/core/presentaion/home_screen.dart
+++ lib/core/presentaion/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:goldy/core/constant/app_colors.dart';
 import 'package:goldy/core/constant/app_strings.dart';
+import 'package:goldy/core/extensions/navigations.dart';
 import 'package:goldy/core/widgets/custom_button.dart';
+
+import '../routing/routes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,13 +18,17 @@ class HomeScreen extends StatelessWidget {
         children: [
           CustomButton(
             text: AppStrings.gold,
-            onPressed: () {},
+            onPressed: () {
+              context.pushNamed(Routes.goldScreen);
+            },
             color: AppColors.goldColor,
           ),
           SizedBox(height: 15),
           CustomButton(
             text: AppStrings.silver,
-            onPressed: () {},
+            onPressed: () {
+              context.pushNamed(Routes.silverScreen);
+            },
             color: AppColors.silverColor,
           ),
         ],
